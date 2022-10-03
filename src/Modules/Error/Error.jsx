75b2911def68fe381510ac4d33ea4319b4b48161
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2022-10-03 05:25:44 +0200

    fixed errorPage

--- src/Modules/Error/Error.jsx
+++ src/Modules/Error/Error.jsx
@@ -12,11 +12,11 @@ const Error = () => {
        
         
         <h1 classname='l'>404</h1>
-        <h3>Seems like you are lost</h3>
-        <h5> or did your parents abandon you here</h5>
+        <h3>Seems like you are lost.</h3>
+        <h5> or did your parents abandon you here?</h5>
         <h6>Oh...   </h6>
         
-        <p>here take a lolipop <a href="/home"> <img src={Lolipop} alt="" className='lolipop'/></a></p>
+        <p>here take a lolipop <a href="/"> <img src={Lolipop} alt="" className='lolipop'/></a></p>
     </div>
   )
 }
